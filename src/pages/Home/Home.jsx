9fe40dd5b850ac0f3e1c986e83d7c16d7b4c4161
Bohdan Hoiko <litvipenko@gmail.com>
2024-01-29 02:02:41 +0200

    initial

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Test = styled.div`
+  font-family: "NoiGrotesk-Light";
+`;
+
+const Home = () => {
+  return <Test>Home</Test>;
+};
+
+export default Home;
